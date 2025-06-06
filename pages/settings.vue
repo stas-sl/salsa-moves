627ccde85e75a8ac29d085acd07c7f1d2c8c552b
Stas Sl <s.slunkov@gmail.com>
2025-06-06 16:27:07 +0300

    add settings page with user ID input and save functionality

---
+++ pages/settings.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto" max-width="400">
+      <v-card-title>Settings</v-card-title>
+      <v-card-text>
+        <v-text-field
+          v-model="uid"
+          label="User ID"
+          outlined
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" @click="saveUid">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const uid = ref('')
+
+onMounted(() => {
+  uid.value = localStorage.getItem('uid') || ''
+})
+
+function saveUid() {
+  localStorage.setItem('uid', uid.value)
+}
+</script>
+
+<style scoped>
+</style>
